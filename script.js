8171470a5b75ea6cab541ff86a2cbff06717d5b5
Breini43 <warfic901@yandex.ru>
2023-07-12 00:35:39 +0300

    writting application with javascript

--- script.js
+++ script.js
@@ -18,13 +18,34 @@
 // //получить остаток от деления через %
 // console.log(5%3);
 
-// // = одиночная - присваивание, двойное == - сравнение, === - строгое сравнение(со сравнением типов)
+// // // = одиночная - присваивание, двойное == - сравнение, === - строгое сравнение(со сравнением типов)
 
-console.log(2+2*2 !== '6');
+// console.log(2+2*2 !== '6');
 
-// //логические операторы и(&&) или(||) && работает только когда оба выражения верны
+// // //логические операторы и(&&) или(||) && работает только когда оба выражения верны
 
-const isChecked = false,
-	  isClose = false;
+// const isChecked = false,
+// 	  isClose = false;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
+
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+
+const personalMovieDatabase = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	  b = prompt('На сколько оцените его?', ''),
+	  c = prompt('Один из последних просмотренных фильмов?', ''),
+	  d = prompt('На сколько оцените его?', '');
+
+	  personalMovieDatabase.movies[a] = b;
+	  personalMovieDatabase.movies[c] = d;
+
+	  console.log(personalMovieDatabase);
+	  
